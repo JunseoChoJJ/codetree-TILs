9d8da23b44df5765585b1e4a05a1807335ff393d
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-13 14:51:53 +0900

    Title: 최대공약수 구하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240313/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+if n <= m:
+    small = n
+else:
+    small = m
+
+for i in range(1, small + 1):
+    if n % i == 0 and m % i == 0:
+        common = i
+
+print(common)
