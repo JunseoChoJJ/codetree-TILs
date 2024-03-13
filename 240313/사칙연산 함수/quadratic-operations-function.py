732754ad7edf80069a78d05992ea5ae1590032ac
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-13 22:49:10 +0900

    Title: 사칙연산 함수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,22 @@
+a, o, c = map(str, input().split())
+
+n1 = int(a)
+n2 = int(c)
+
+def check(n1, n2,a,o,c):
+    ans = 0
+    if o == "+":
+        ans = n1 + n2
+        print(a + " " + o + " " + c + " = " + str(ans) )
+    elif o == "-":
+        ans = n1 - n2
+        print(a + " " + o + " " + c + " = " + str(ans) )
+    elif o == "*":
+        ans = n1 * n2
+        print(a + " " + o + " " + c + " = " + str(ans) )
+    elif o == "/":
+        ans = n1 / n2
+        print(a + " " + o + " " + c + " = " + str(ans) )
+    else:
+        return False
+check(n1, n2,a,o,c)
