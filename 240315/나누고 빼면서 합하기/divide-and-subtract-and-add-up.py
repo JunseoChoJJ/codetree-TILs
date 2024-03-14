39aa83db09c05f0a4921a9b83b0c44f5932757b1
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-15 00:47:27 +0900

    Title: 나누고 빼면서 합하기, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240315/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
+
+def check(n, m, n_list):
+    
+    total = n_list[m-1]
+    
+    while True:
+        if m == 0:
+            break
+        if m % 2 == 0:
+            m //= 2
+            total += n_list[m-1]
+        else:
+            m -= 1
+            total += n_list[m-1]
+    return total
+
+
+print(check(n, m, n_list))
