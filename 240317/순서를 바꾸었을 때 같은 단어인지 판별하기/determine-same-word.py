7894552e8fcac9c862791ecc37262415b497deeb
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-17 00:57:38 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240317/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -0,0 +1,27 @@
+a_list = list(input())
+b_list = list(input())
+
+a_list.sort()
+b_list.sort()
+
+
+def equal():
+    global a_list
+    global b_list
+
+    a1 = len(a_list)
+    b1 = len(b_list)
+
+    if a1 >= b1:
+        num = a1
+    else:
+        num = b1
+    for i in range(num):
+        if a_list[i] != b_list[i]:
+            return False
+    return True
+
+if equal():
+    print("Yes")
+else:
+    print("No")
