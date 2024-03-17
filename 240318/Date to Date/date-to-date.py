b4216f1a0a5b907b99be9a3886e6ff628fc9d070
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-18 00:27:11 +0900

    Title: Date to Date, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240318/Date to Date/date-to-date.py
@@ -0,0 +1,20 @@
+m1,d1,m2,d2 = map(int, input().split())
+
+ans = 0
+month = m1
+day = d1
+
+num_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+while True:
+    if month == m2 and day == d2:
+        break
+    
+    ans += 1
+    day += 1
+
+    if day > num_days[month]:
+        month += 1
+        day = 0
+    
+print(ans)
