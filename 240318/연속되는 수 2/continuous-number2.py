9cb3f08f8dd879300d79de1300634c080f2e2a83
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-18 23:55:16 +0900

    Title: 연속되는 수 2, Time: 96ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/연속되는 수 2/continuous-number2.py
+++ 240318/연속되는 수 2/continuous-number2.py
@@ -6,12 +6,13 @@ cnt = 0
 tmp = 0
 
 for j in range(n):
-    if j == 0:
-        cnt = 1
     if j == 0 or a[j] != a[j-1]:
         if cnt > tmp:
             tmp = cnt
         cnt = 1
     else:
         cnt += 1
-print(tmp)
+if n == 1:
+    print(1)
+else:
+    print(tmp)
