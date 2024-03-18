d4c0544c563be847db9adbf2e3e6ac94f2fce15e
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-19 02:22:51 +0900

    Title: 1이 3개 이상 있는 위치, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240319/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240319/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -6,19 +6,23 @@ for _ in range(n):
 dy = [0,0,-1,1]
 dx = [-1,1,0,0]
 ans = 0
-for i in range(n):
-    for j in range(n):
-        
-        ny = i
-        nx = j
-        cnt = 0
-        for k in range(n):
-            ny1 = ny + dy[k]
-            nx1 = nx + dx[k]
+ny, nx = 0, 0
+while True:
+    cnt = 0
+    for k in range(4):
+        ny1 = ny + dy[k]
+        nx1 = nx + dx[k]
 
-            if ny1 < 0 or nx1 < 0 or ny1 >= n or nx1 >= n: continue
-            if board[ny1][nx1] == 1:
-                cnt += 1
-        if cnt >= 3:
-            ans += 1
+        if ny1 < 0 or nx1 < 0 or ny1 >= n or nx1 >= n: continue
+        if board[ny1][nx1] == 1:
+            cnt += 1
+    if cnt >= 3:
+        ans += 1        
+    if ny == (n - 1) and nx == (n - 1):
+        break
+    if nx == (n - 1):
+        nx = 0
+        ny += 1
+    else:
+        nx += 1
 print(ans)
