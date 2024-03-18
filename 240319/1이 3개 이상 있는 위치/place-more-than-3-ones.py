b340dd16e348d1d2fb69f81410d1f1d8cf33802d
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-19 02:12:27 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,24 @@
+n = int(input())
+board = []
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+dy = [0,0,-1,1]
+dx = [-1,1,0,0]
+ans = 0
+for i in range(n):
+    for j in range(n):
+        
+        ny = i
+        nx = j
+        cnt = 0
+        for k in range(n):
+            ny1 = ny + dy[k]
+            nx1 = nx + dx[k]
+
+            if ny1 < 0 or nx1 < 0 or ny1 >= n or nx1 >= n: continue
+            if board[ny1][nx1] == 1:
+                cnt += 1
+        if cnt >= 3:
+            ans += 1
+print(ans)
