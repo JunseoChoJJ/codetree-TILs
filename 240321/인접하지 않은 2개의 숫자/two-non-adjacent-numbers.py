20b2860b17ae510b1ee16f460c1be92edab55a3f
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-21 22:12:21 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,10 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+ans = 0
+for i in range(len(n_list)):
+    total = 0
+    for j in range(i+2, len(n_list)):
+        total = n_list[i] + n_list[j]
+    ans = max(ans, total)
+print(ans)
