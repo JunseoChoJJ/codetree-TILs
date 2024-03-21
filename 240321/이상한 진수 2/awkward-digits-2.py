92f86dfa527ea0e7c8a16828831566598a910884
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-21 17:00:30 +0900

    Title: 이상한 진수 2, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,21 @@
+string = list(input())
+ans = 0
+
+def check(string):
+    num = 0
+    string = string[::-1]
+    for i in range(len(string)):
+        if string[i] == '1':
+            num += 2**i 
+    return num
+
+for i in range(len(string)):
+    if string[i] == '0':
+        string[i] = '1'
+        num = check(string)
+        ans = max(ans, num)
+        string[i] = '0'
+    else:
+        num = check(string)
+        ans = max(ans, num)
+print(ans)
