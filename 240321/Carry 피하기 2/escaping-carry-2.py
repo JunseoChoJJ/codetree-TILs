ef2b1af51bda55e01cd6c36b8f630a1be6807948
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-21 22:25:30 +0900

    Title: Carry 피하기 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,24 @@
+n = int(input())
+n_list = []
+
+for _ in range(n):
+    n_list.append(int(input()))
+
+ans = 0
+
+def check(n1, n2, n3):
+    if (n1 % 10 + n2 % 10 + n3 % 10) > 9:
+        return False
+    if n1 == 0 and n2 == 0 and n3 == 0:
+        return True
+    return check(n1 // 10, n2 //10, n3 // 10)
+
+for i in range(n):
+    total = 0
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if check(n_list[i],n_list[j], n_list[k]):
+                total += n_list[i] + n_list[j] + n_list[k]
+                ans = max(ans, total)
+
+print(ans)
