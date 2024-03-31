1b0812e152f1a936c14f11de9717f24cc19f350c
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-03-31 21:01:04 +0900

    Title: 선택 정렬 구현, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240331/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,18 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+def insertion(n_list):
+
+    for i in range(len(n_list) - 1):
+        mins = i
+        for j in range(i, len(n_list) - 1):
+            if n_list[j] < n_list[mins]:
+                mins = j
+        tmp = n_list[i]
+        n_list[i] = n_list[mins]
+        n_list[mins] = tmp
+        #print(n_list)
+insertion(n_list)
+
+for num in n_list:
+    print(num, end = ' ')
