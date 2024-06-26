e1e4812c990da0be09a99dad43c17dcdf32b0073
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-06-26 18:15:52 +0900

    Title: 총점 비교, Time: 168ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240626/총점 비교/compare-total-points.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+students = []
+
+n = int(input())
+for _ in range(n):
+    string = list(map(str, input().split()))
+    students.append(Student(string[0], int(string[1]), int(string[2]), int(string[3])))
+
+
+students.sort(key=lambda x: x.kor + x.eng + x.math)
+
+for i in range(n):
+    print(students[i].name, students[i].kor, students[i].eng, students[i].math)
