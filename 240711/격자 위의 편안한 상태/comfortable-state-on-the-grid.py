3720bdb2697ec3bb43c9e86c5fc68930fa72231a
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-11 22:42:44 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240711/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+board = [[0] * m for _ in range(n)]
+
+dy = [0,0,-1,1]
+dx = [-1,1,0,0]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    r, c = r-1, c-1
+    
+    cnt = 0
+    board[r][c] = 1
+    for i in range(4):
+        nr = r + dy[i]
+        nc = c + dx[i]
+
+        if nr < 0 or nc < 0 or nr >= n or nc >= m: continue
+        if board[nr][nc] == 1:
+            cnt += 1
+    if cnt == 3:
+        print(1)
+    else:
+        print(0)
