f592a94cb9c84f679938b613e0197743057b51be
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-15 20:23:12 +0900

    Title: 개발팀의 능력, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,21 @@
+import sys
+n_list = list(map(int, input().split()))
+
+def diff(sum1, sum2, sum3):
+    return abs(max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
+
+ans = sys.maxsize
+
+for i in range(5):
+    for j in range(i+1, 5):
+        
+        for k in range(5):
+            for l in range(k+1, 5):
+                if i == k or i == l or j == k or j == l: continue
+                sum1 = n_list[i] + n_list[j]
+                sum2 = n_list[k] + n_list[l]
+                sum3 = sum(n_list) - sum1 - sum2
+                if sum1 == sum2 or sum2 == sum3 or sum1 == sum3: continue
+                ans = min(ans, diff(sum1, sum2, sum3))
+
+print(ans)
