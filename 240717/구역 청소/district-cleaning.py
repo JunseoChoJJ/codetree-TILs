f5f5071f672103a35572617ee3e801e96a3b43c4
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-17 21:49:06 +0900

    Title: 구역 청소, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/구역 청소/district-cleaning.py
+++ 240717/구역 청소/district-cleaning.py
@@ -3,13 +3,13 @@ c, d = map(int, input().split())
 
 check = [0] * 101
 
-for i in range(a, b+1):
+for i in range(a, b):
     check[i] = 1
-for j in range(c, d+1):
+for j in range(c, d):
     check[j] = 1
 ans = 0
 for k in range(0, 101):
     if check[k] == 1:
         ans += 1
 
-print(ans-1)
+print(ans)
