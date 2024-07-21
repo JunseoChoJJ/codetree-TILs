aa0e681350b8706fca72dbcfb2eb4c2ee1911856
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-21 18:44:19 +0900

    Title: 두 번째로 작은 수의 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240721/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240721/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -7,13 +7,17 @@ n_sort = sorted(n_list)
 
 check = n_sort[0]
 quick = False
+
 for i in range(n):
     if n_sort[i] !=  check:
         quick = True
         ans = n_sort[i]
+        break
+
 if quick == False:
     print(-1)
 cnt = 0
+
 for j in range(n):
     if n_list[j] == ans:
         hi = j
