ec9b1fe2b72b2069407e2285334bc8c5b4e7734b
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-30 17:20:48 +0900

    Title: 트로미노, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/트로미노/tromino.py
+++ 240730/트로미노/tromino.py
@@ -9,18 +9,21 @@ max_ans = 0
 
 for row in range(n):
     for column in range(m):
-        if row + 1 >=n or column + 1 >= m: continue
-        block1 = board[row][column] + board[row+1][column] + board[row+1][column+1]
-        block2 = board[row][column] + board[row][column+1] + board[row+1][column]
-        block3 = board[row][column] + board[row][column+1] + board[row+1][column+1]
-        block4 = board[row][column+1] + board[row+1][column+1] + board[row+1][column]
-        max_ans = max(max_ans, block1,block2,block3,block4)
-        if row + 2 >= n and column + 2 >= m: continue
-        block5, block6 = 0,0
-        if row + 2 >= n and column + 2 < m:
+        if row + 1 < n and column + 1 < m:
+            block1 = board[row][column] + board[row+1][column] + board[row+1][column+1]
+            block2 = board[row][column] + board[row][column+1] + board[row+1][column]
+            block3 = board[row][column] + board[row][column+1] + board[row+1][column+1]
+            block4 = board[row][column+1] + board[row+1][column+1] + board[row+1][column]
+        if row + 2 < n and column + 2 < m:
             block5 = board[row][column] + board[row][column+1] + board[row][column+2]
-        if row + 2 <= n and column + 2 > m:
-            block6 = board[row][column] + board[row+1][column] + board[row+2][column]                                          
-        max_ans = max(max_ans, block5, block6)
+            block6 = board[row][column] + board[row+1][column] + board[row+2][column]
+        if row + 2 < n and column+2 >= m:
+            block6 = board[row][column] + board[row+1][column] + board[row+2][column]
+            block5 = 0
+        if row+2 >= n and column+2 < m:
+            block5 = board[row][column] + board[row][column+1] + board[row][column+2]
+            block6 = 0
+        
+        max_ans = max(max_ans, block1,block2, block3,block4, block5, block6)
 
 print(max_ans)
