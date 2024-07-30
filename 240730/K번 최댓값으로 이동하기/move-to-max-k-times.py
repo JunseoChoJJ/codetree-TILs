f4cbbdf68b96cd7e87feee2f160699e8f1b99da0
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-07-30 19:11:42 +0900

    Title: K번 최댓값으로 이동하기, Time: 160ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240730/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,57 @@
+from collections import deque
+n, k = map(int, input().split())
+board=[]
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+startY, startX = map(int, input().split())
+
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+
+
+
+def startLocation(max_num):
+    arr=[]
+    for i in range(n): 
+        for j in range(n):
+            if board[i][j] == max_num:
+                return i,j
+
+check = True
+for l in range(k):
+    # cannot move before kth turn 
+    if check == False:
+        break
+    q = deque([(startY-1, startX-1)])
+    first = board[startY-1][startX-1]
+    
+    max_num = -1
+    visited = [[0] * n for _ in range(n)]
+    visited[startY-1][startX-1] = 1
+    while q:
+        (nowY, nowX) = q.popleft()
+        for i in range(4):
+            ny = nowY + dy[i]
+            nx = nowX + dx[i]
+
+            if ny < 0 or nx < 0 or ny >= n or nx >= n: continue
+            if board[ny][nx] >= first: continue
+            if visited[ny][nx] == 1: continue
+            visited[ny][nx] = 1
+            max_num = max(max_num, board[ny][nx])
+            q.append((ny, nx))
+
+    check2 = False
+    for i in range(n): 
+        if check2 == True:
+            break
+        for j in range(n):
+            if board[i][j] == max_num:
+                startY = i+1
+                startX = j+1
+                check2 = True
+                break
+
+    
+print(startY, startX)
