1fe9e7bece2684d30f2945efa23782a65acaf229
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-08-07 18:42:17 +0900

    Title: 피보나치 수, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/피보나치 수/fibonacci-number.py
+++ 240807/피보나치 수/fibonacci-number.py
@@ -1,14 +1,10 @@
-n = 2
+n = int(input())
 dp = [0] * 46
-
-def solution(n):
-    dp[1] = 1
-    dp[2] = 1
-
-    for i in range(3, n+1):
-        dp[i] = dp[i-1] + dp[i-2]
-
-    return dp[n]
+dp[1] = 1
+dp[2] = 1
 
 
-print(solution(n))
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
