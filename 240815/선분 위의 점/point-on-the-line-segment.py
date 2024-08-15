4531df3cde340c6b382f7ae08b7e3e9e5058200f
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-08-15 19:20:23 +0900

    Title: 선분 위의 점, Time: 5007ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240815/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
+
+
+def binary_search(a, b):
+    cnt = 0
+
+    for num in n_list:
+        left = a
+        right = b
+        
+        
+        while left <= right:
+            mid = (left + right) // 2    
+
+            if mid == num:
+                cnt += 1
+                break
+            if mid > num:
+                right = mid - 1
+            else:
+                left = mid + 1 
+    
+    return cnt
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(binary_search(a,b))
