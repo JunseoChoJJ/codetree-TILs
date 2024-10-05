bf56aaba1db7f315ffbcd610541ffe9992fc4ae0
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-10-05 18:10:08 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 143ms, Memory: 31MB, Status: Passed - CodeTree

--- 241005/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 241005/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -7,10 +7,15 @@ total = 0
 possibleAns = []
 for num in nList:
     if num < 0:
-        possibleAns.append(total)
+        if total > 0:
+            possibleAns.append(total)
     if total < 0:
         total = 0
     total += num
+
+
 possibleAns.append(total)
+
+
 
 print(max(possibleAns))
