4c30dfe28d77f21e2bc51729a87de8060f9c5acd
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-10-16 20:38:11 +0900

    Title: 데이터 비교, Time: 245ms, Memory: 40MB, Status: Passed - CodeTree

--- 241016/데이터 비교/data-comparison.py
+++ 241016/데이터 비교/data-comparison.py
@@ -3,6 +3,7 @@ nList = list(map(int, input().split()))
 m = int(input())
 mList = list(map(int, input().split()))
 
+nList = set(nList)
 for num in mList:
     if num in nList:
         print(1, end = ' ')
