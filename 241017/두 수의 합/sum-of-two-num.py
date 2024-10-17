8f8628ff18c36e2ad4f0660d27f91358c9855028
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-10-17 18:28:36 +0900

    Title: 두 수의 합, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,18 @@
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+d = dict()
+
+for i in range(n):
+    if arr[i] not in d:
+        d[arr[i]] = 1
+
+
+cnt = 0
+for num in arr:
+    remain = k - num
+    if remain in d:
+        cnt += 1
+
+print(cnt // 2)
