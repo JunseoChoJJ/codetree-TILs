5a5ba11b264f3d5e812e90207ba3e161379d9739
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-10-17 21:04:44 +0900

    Title: 특별한 문자, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/특별한 문자/special-character.py
@@ -0,0 +1,23 @@
+string = list(input())
+
+
+di = dict()
+
+for char in string:
+    if char not in di:
+        di[char] = 1
+    else:
+        di[char] += 1
+
+
+check = False
+
+for key, value in di.items():
+    if value == 1:
+        ans = key
+        check = True
+
+if check == False:
+    print("None")
+else:
+    print(ans)
