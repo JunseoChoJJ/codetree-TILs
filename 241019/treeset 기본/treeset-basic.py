9e4da5bf4fb0cb464c96e37b361c37051fb70d9f
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2024-10-19 00:06:51 +0900

    Title: treeset 기본, Time: 1153ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 241019/treeset 기본/treeset-basic.py
@@ -0,0 +1,48 @@
+from sortedcontainers import SortedSet
+s = SortedSet()
+
+n = int(input())
+
+
+
+for _ in range(n):
+    command = list(map(str, input().split(" ")))
+
+    if command[0] == "add":
+        x = command[1]
+        s.add(x)
+    elif command[0] == "remove":
+        x = command[1]
+        s.remove(x)
+    elif command[0] == "find":
+        x = command[1]
+        if x in s:
+            print("true")
+        else:
+            print("false")
+    elif command[0] == "lower_bound":
+        x = command[1]
+        idx = s.bisect_left(x)
+        if idx < len(s):
+            print(s[idx])
+        else:
+            print("None")
+            
+    elif command[0] == "upper_bound":
+        x = command[1]
+        idx = s.bisect_right(x)
+        if idx < len(s):
+            print(s[idx])
+        else:
+            print("None")
+            
+    elif command[0] == "largest":
+        if not s:
+            print("None")
+        else:
+            print(s[-1])
+    else:
+        if not s:
+            print("None")
+        else:
+            print(s[0])
