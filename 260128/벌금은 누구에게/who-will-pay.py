099a21208630c816b21b78e04482ce79ac6645de
Junseo Cho <chojunseo1@naver.com>
2026-01-28 21:13:24 +0900

    Title: 벌금은 누구에게, Time: 68ms, Memory: 16MB, Status: Passed - Codetree

--- 260128/벌금은 누구에게/who-will-pay.py
+++ 260128/벌금은 누구에게/who-will-pay.py
@@ -4,12 +4,15 @@ student = [int(input()) for _ in range(M)]
 # Please write your code here.
 
 num = [0] * (N+1)
-
+check = False
 for st in student:
     num[st] += 1
 
     if num[st] == K:
         print(st)
-
+        check = True
         break
+
+if check == False:
+    print(-1)
 
