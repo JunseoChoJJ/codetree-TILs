a73c7a6d6db54d45de2d23c790b8838daf551810
Junseo Cho <chojunseo1@naver.com>
2026-01-31 18:51:24 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 71ms, Memory: 18MB, Status: Passed - Codetree

--- 260131/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 260131/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -12,33 +12,35 @@ dxs = [0,-1,0,1]
 dys = [1,0,-1,0]
 direction =0
 
-num = 1
-grid[startX][startY] = num
-
-startX += dxs[direction]
-startY += dys[direction]
-num += 1
-grid[startX][startY] = num
-
-for i in range(2, n*n):
-
-    direction = (direction + 1) % 4
-    nx = startX + dxs[direction]
-    ny = startY + dys[direction]
-    num += 1
-    
 
 
-    if grid[nx][ny] == 0:
-            grid[nx][ny] = num
+for i in range(n*n):
+    if i == 0:
+        num = 1
+        grid[startX][startY] = num
+    elif i == 1:
+        startX += dxs[direction]
+        startY += dys[direction]
+        num += 1
+        grid[startX][startY] = num
     else:
-        direction = (direction -1 + 4) % 4
+        direction = (direction + 1) % 4
         nx = startX + dxs[direction]
         ny = startY + dys[direction]
-        grid[nx][ny] = num
+        num += 1
         
-    startX = nx
-    startY = ny
+
+
+        if grid[nx][ny] == 0:
+                grid[nx][ny] = num
+        else:
+            direction = (direction -1 + 4) % 4
+            nx = startX + dxs[direction]
+            ny = startY + dys[direction]
+            grid[nx][ny] = num
+            
+        startX = nx
+        startY = ny
     
 for i in range(n):
     for j in range(n):
